4182800dd0378850f16392ee4d17056cf4409350
Shivam Nagpal <shivamnagpal432@gmail.com>
2019-03-29 00:51:28 +0530

    Updated

--- 07.02.RoundRobin.java
+++ 07.02.RoundRobin.java
@@ -3,13 +3,10 @@
 Shortest remaining time and Round Robin (RR) scheduling algorithms. Experiment
 with different quantum sizes for RR algorithm.
 
-SJF Part in this file
+Round Robin Part in this file
 */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 class Process implements Comparable<Process> {
     public int processId;
@@ -28,23 +25,23 @@ class Process implements Comparable<Process> {
 
     @Override
     public int compareTo(Process p) {
-        if (this.arrivalTime == p.arrivalTime) {
-            return Integer.compare(this.burstTime, p.burstTime);
-        } else {
-            return Integer.compare(this.arrivalTime, p.arrivalTime);
-        }
+        return Integer.compare(this.arrivalTime, p.arrivalTime);
     }
 }
 
-class SJF {
+class RoundRobin {
 
+    private static int quantumSize;
     private static Process[] processes;
     private static TreeSet<Integer> arrivalTimes;
-    private static ArrayList<Process> arrivedProcesses;
+    private static Queue<Process> arrivedProcesses;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Quantum Size");
+        quantumSize = scanner.nextInt();
+
         System.out.println("Enter number of Processes");
         int n = scanner.nextInt();
         processes = new Process[n];
@@ -66,10 +63,10 @@ class SJF {
             arrivalTimes.add(processes[i].arrivalTime);
         }
 
-        arrivedProcesses = new ArrayList<>();
+        arrivedProcesses = new LinkedList<>();
 
         System.out.println("Process Scheduling");
-        sjf();
+        roundRobin();
 
         float totalWaitingTime = 0f;
         System.out.println("\nWaiting Time");
@@ -81,54 +78,46 @@ class SJF {
         System.out.println("\nAverage Waiting Time: " + (totalWaitingTime / n));
     }
 
-    private static void sjf() {
+    private static void roundRobin() {
         int processCursor = 0;
         int currentTime = 0;
-        boolean checkForNextMinimumProcess = false;
-        Process minProcess = null;
+        Process currentProcess = null;
 
         while (true) {
 
             if (arrivalTimes.contains(currentTime)) {
                 while (processCursor < processes.length && processes[processCursor].arrivalTime <= currentTime) {
-                    arrivedProcesses.add(processes[processCursor]);
+                    arrivedProcesses.offer(processes[processCursor]);
                     processCursor += 1;
                 }
-                checkForNextMinimumProcess = true;
             }
 
-            if (checkForNextMinimumProcess) {
-                int minRemainingTime = Integer.MAX_VALUE;
-                for (Process p : arrivedProcesses) {
-                    if (p.remainingTime < minRemainingTime) {
-                        minProcess = p;
-                        minRemainingTime = p.remainingTime;
-                    }
-                }
+            currentProcess = arrivedProcesses.poll();
 
-                checkForNextMinimumProcess = false;
+            int waitTime = 0;
+
+            if (currentProcess != null) {
+                System.out.println("Current Time: " + currentTime + " , Process: " + currentProcess.processId);
+
+                if (currentProcess.remainingTime > quantumSize) {
+                    currentTime += quantumSize;
+                    waitTime = quantumSize;
+                    currentProcess.remainingTime -= quantumSize;
+                    arrivedProcesses.offer(currentProcess);
+                } else {
+                    currentTime += currentProcess.remainingTime;
+                    waitTime = currentProcess.remainingTime;
+                    currentProcess.remainingTime = 0;
+                    arrivedProcesses.remove(currentProcess);
+                }
             }
 
             for (Process p : arrivedProcesses) {
-                if (p == minProcess) {
+                if (p == currentProcess) {
                     continue;
                 }
-                p.waitingTime += 1;
+                p.waitingTime += waitTime;
             }
-
-            if (minProcess != null) {
-                minProcess.remainingTime -= 1;
-
-                System.out.println("Current Time: " + currentTime + " , Process: " + minProcess.processId);
-
-                if (minProcess.remainingTime == 0) {
-                    arrivedProcesses.remove(minProcess);
-                    checkForNextMinimumProcess = true;
-                    minProcess = null;
-                }
-            }
-
-            currentTime += 1;
 
             if (arrivedProcesses.isEmpty() && currentTime > arrivalTimes.last()) {
                 break;
@@ -141,63 +130,56 @@ class SJF {
 /*
 Output:
 
+Enter Quantum Size
+2
 Enter number of Processes
-4
+5
 Enter details
 Enter arrival time for Process 1
 0
 Enter burst time for Process 1
-8
+10
 
 Enter arrival time for Process 2
-1
+0
 Enter burst time for Process 2
-4
+1
 
 Enter arrival time for Process 3
-2
+3
 Enter burst time for Process 3
-9
+2
 
 Enter arrival time for Process 4
-3
+5
 Enter burst time for Process 4
+1
+
+Enter arrival time for Process 5
+10
+Enter burst time for Process 5
 5
 
 Process Scheduling
-CurrentTime: 0 , Process: 1
-CurrentTime: 1 , Process: 2
-CurrentTime: 2 , Process: 2
-CurrentTime: 3 , Process: 2
-CurrentTime: 4 , Process: 2
-CurrentTime: 5 , Process: 4
-CurrentTime: 6 , Process: 4
-CurrentTime: 7 , Process: 4
-CurrentTime: 8 , Process: 4
-CurrentTime: 9 , Process: 4
-CurrentTime: 10 , Process: 1
-CurrentTime: 11 , Process: 1
-CurrentTime: 12 , Process: 1
-CurrentTime: 13 , Process: 1
-CurrentTime: 14 , Process: 1
-CurrentTime: 15 , Process: 1
-CurrentTime: 16 , Process: 1
-CurrentTime: 17 , Process: 3
-CurrentTime: 18 , Process: 3
-CurrentTime: 19 , Process: 3
-CurrentTime: 20 , Process: 3
-CurrentTime: 21 , Process: 3
-CurrentTime: 22 , Process: 3
-CurrentTime: 23 , Process: 3
-CurrentTime: 24 , Process: 3
-CurrentTime: 25 , Process: 3
-Current Time: 26
+Current Time: 0 , Process: 1
+Current Time: 2 , Process: 2
+Current Time: 3 , Process: 1
+Current Time: 5 , Process: 3
+Current Time: 7 , Process: 1
+Current Time: 9 , Process: 4
+Current Time: 10 , Process: 1
+Current Time: 12 , Process: 5
+Current Time: 14 , Process: 1
+Current Time: 16 , Process: 5
+Current Time: 18 , Process: 5
+Current Time: 19
 
 Waiting Time
-Process 1, Waiting Time: 9
-Process 2, Waiting Time: 0
-Process 3, Waiting Time: 15
-Process 4, Waiting Time: 2
+Process 1, Waiting Time: 6
+Process 2, Waiting Time: 2
+Process 3, Waiting Time: 2
+Process 4, Waiting Time: 4
+Process 5, Waiting Time: 4
 
-Average Waiting Time: 6.5
+Average Waiting Time: 3.6
 */
